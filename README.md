cd6149c79a9813c4076598639ddfb0e4b518dde1
Misyuriy <andrey.misyurev@yandex.ru>
2024-02-28 16:53:30 +0300

    optimized for loops

--- README.md
+++ README.md
@@ -21,17 +21,17 @@ The program accepts **Equation** syntax:
 Each compound (reagent or product) must be a valid SMILES string.
 To get valid SMILES representation, find compound on PubChem: https://pubchem.ncbi.nlm.nih.gov/
 
-Example reaction: formaldehyde with 2-methoxyethanol
+Example reaction: benzaldehyde with acetic acid
 
-    COCCO & C=O >>> COCCOCOCCOC
+    C(C)(=O)O & C(C1=CC=CC=C1)=O >>> C=C=O & C=C1CC(=O)O1
 
 ## Usage in code:
-Prediction of catalyst for formaldehyde reaction with 2-methoxyethanol:
+Prediction of catalyst for benzaldehyde reaction with acetic acid:
 
     from ordp.predict import predict_catalyst
     from ordp.convert_reaction import convert_equation
     
-    data = convert_equation('COCCO & C=O >>> COCCOCOCCOC')
+    data = convert_equation('C(C)(=O)O & C(C1=CC=CC=C1)=O >>> C=C=O & C=C1CC(=O)O1')
     
     print(predict_catalyst(data))
 
@@ -46,3 +46,5 @@ Work in progress:
 - More features for usage in code
 
 NOTE: temperature prediction accuracy is low.
+
+NOTE 2: the models are almost useless for inorganic reactions. Predictions will be random
--- convert_reaction.py
+++ convert_reaction.py
@@ -47,20 +47,14 @@ def convert_equation(equation: str, max_reactants=8, max_products=6):
 def get_known_equation(equation: str, max_reactants=8, max_products=6):
     r, p = equation.split('>>>')
     reactants = r.split('&')
-    products = p.split('&')
 
     result = {}
     for i in range(max_reactants):
         result['reactant_' + str(i)] = -1
-    for i in range(max_products):
-        result['product_' + str(i)] = -1
 
     for index, reactant in enumerate(reactants):
         reactants[index] = Chem.CanonSmiles(reactant.strip(' '))
-    for index, product in enumerate(products):
-        products[index] = Chem.CanonSmiles(product.strip(' '))
 
-    products = set([Chem.CanonSmiles(i) for i in products])
     reactants = set([Chem.CanonSmiles(i) for i in reactants])
 
     paths = ['known/known_more_products.csv',
@@ -70,16 +64,16 @@ def get_known_equation(equation: str, max_reactants=8, max_products=6):
     for path in paths:
         known = pd.read_csv(path)
 
-        for row in known.iloc:
-            known_reactants = [row['reactant_' + str(i)] for i in range(max_reactants)]
-            known_reactants = set([(Chem.CanonSmiles(i) if type(i) == str else i) for i in known_reactants])
+        known_reactants = known[['reactant_' + str(i) for i in range(max_reactants)]].applymap(
+            lambda x: Chem.CanonSmiles(x) if isinstance(x, str) else x)
+        known_reactants = known_reactants.apply(lambda x: set(x.dropna()), axis=1)
 
-            if None in known_reactants:
-                known_reactants.remove(None)
-            if np.nan in known_reactants:
-                known_reactants.remove(np.nan)
+        def compare_reactants(known_reactants, reactants):
+            return known_reactants == set(reactants)
 
-            if known_reactants == set(reactants):
-                return row['temperature'], row['solvent_0'], row['catalyst_0']
+        matches = known[known_reactants.apply(lambda x: compare_reactants(x, reactants))]
+
+        if not matches.empty:
+            return matches['temperature'].iloc[0], matches['solvent_0'].iloc[0], matches['catalyst_0'].iloc[0]
 
     return None, None, None
